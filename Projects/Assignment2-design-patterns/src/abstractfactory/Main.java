ec89957db7a531289f6c3590a8338caa8de067ef
AltynbekTuT <253689@astanait.edu.kz>
2026-09-21 15:46:36 +0500

    Configure of main

--- Projects/Assignment2-design-patterns/src/abstractfactory/Main.java
+++ Projects/Assignment2-design-patterns/src/abstractfactory/Main.java
@@ -1,3 +1,22 @@
+package abstractfactory;
+
+import abstractfactory.factories.ForestWorldFactory;
+import abstractfactory.factories.IceWorldFactory;
+import abstractfactory.factories.WorldFactory;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        // Конфигурация мира меняется ровно в одном месте
+        String selectedBiome = "ICE"; // Можно подставить "FOREST"
+
+        WorldFactory factory;
+        if ("FOREST".equalsIgnoreCase(selectedBiome)) {
+            factory = new ForestWorldFactory();
+        } else {
+            factory = new IceWorldFactory();
+        }
+
+        GameWorldClient client = new GameWorldClient(factory);
+        client.loadWorld();
+    }
 }
