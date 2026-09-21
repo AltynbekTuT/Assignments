93fc7e579f53e5a60fd1e0810b9a431b048ac97a
AltynbekTuT <253689@astanait.edu.kz>
2026-09-21 15:46:01 +0500

    Game world factory coding

--- Projects/Assignment2-design-patterns/src/abstractfactory/GameWorldClient.java
+++ Projects/Assignment2-design-patterns/src/abstractfactory/GameWorldClient.java
@@ -1,3 +1,27 @@
+package abstractfactory;
+
+import abstractfactory.components.Enemy;
+import abstractfactory.components.Soundtrack;
+import abstractfactory.components.Terrain;
+import abstractfactory.factories.WorldFactory;
+
 public class GameWorldClient {
-    
+    private final Enemy enemy;
+    private final Terrain terrain;
+    private final Soundtrack soundtrack;
+
+    // Внедрение фабрики через конструктор (композиция)
+    public GameWorldClient(WorldFactory factory) {
+        this.enemy = factory.createEnemy();
+        this.terrain = factory.createTerrain();
+        this.soundtrack = factory.createSoundtrack();
+    }
+
+    public void loadWorld() {
+        System.out.println("Loading level components...");
+        terrain.render();
+        soundtrack.play();
+        enemy.spawn();
+        System.out.println("Level loaded successfully.\n");
+    }
 }
